9fa4f53338b526511b2606c5b48b3b3bc646de16
Spjut-Audrey <31967527+Spjut-Audrey@users.noreply.github.com>
2019-11-30 23:22:50 -0500

    uploading milestone 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var usd = 1;
-var nzd = 0.64;
+var bodyParser = require('body-parser');
+// var usd = 1;
+// var nzd = 0.64;
 
 function compute(){
     var mysql      = require('mysql');
@@ -12,12 +13,45 @@ function compute(){
 
     connection.connect();
 
+
+    var request = new XMLHttpRequest();
+
     connection.query('SELECT * from heroku_ca91be7a53f5ea4.currency WHERE curr_name = nzd', function(err, rows, fields) {
     if (!err)
-        console.log('The solution is: ', row[2]);
+        // get number from user
+        request.open("POST", "currency.html?number");
+        
+        //once ready add to result area
+        request.onreadystatechange = function() {
+            // Check if the request is complete and was successful
+            if(this.readyState === 4 && this.status === 200) {
+                // Inserting response from server into result div
+                bodyParser.urlencoded({ extended: false })
+                var resultNum = req.body.ID;
+                document.getElementById("result").innerHTML = resultNum;
+            }
+
+            // send data out
+        request.send()
     else
         console.log('Error while performing Query.');
     });
 
     connection.end();
 }
+
+// ajax code from https://stackoverflow.com/questions/45753417/ajax-call-returns-values-and-my-html-page
+// useful?
+// $.ajax({
+//     type: "POST",
+//     url: "@Url.Action("compute", "Item")",
+//     data: '{ "imageData" : "' + image + '" }',
+//     contentType: "application/json; charset=utf-8",
+//     dataType: "json",
+//     success: function (success) {
+//         alert('Success ' + success.responseText);
+//     },
+//     error: function (response) {
+//         alert(response.responseText);
+//     }
+// });
